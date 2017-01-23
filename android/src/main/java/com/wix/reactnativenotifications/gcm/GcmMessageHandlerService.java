94bd7df86c7510cdbd60d43372b9b2481b9b20cd
Jonathan Stanton <jonathan@jastanton.com>
2017-01-23 15:18:37 -0800

    transform PN from convoys mutilation to what it should be

--- android/src/main/java/com/wix/reactnativenotifications/gcm/GcmMessageHandlerService.java
+++ android/src/main/java/com/wix/reactnativenotifications/gcm/GcmMessageHandlerService.java
@@ -2,6 +2,7 @@ package com.wix.reactnativenotifications.gcm;
 
 import android.os.Bundle;
 import android.util.Log;
+import org.json.*;
 
 import com.google.android.gms.gcm.GcmListenerService;
 import com.wix.reactnativenotifications.core.notification.IPushNotification;
@@ -14,6 +15,31 @@ public class GcmMessageHandlerService extends GcmListenerService {
     @Override
     public void onMessageReceived(String s, Bundle bundle) {
         Log.d(LOGTAG, "New message from GCM: " + bundle);
+        String rawData = bundle.getString("data");
+        // Hack by Convoy, all of our data is nested in "data" json. We need to bring it up a level.
+        // we could change this in API but it's backwards incompatible with current app to do so.
+        if (rawData.length() > 0) {
+            try {
+                JSONObject data = new JSONObject(rawData);
+                try {
+                    bundle.putString("title", data.getString("title"));
+                } catch (JSONException ignored) {}
+                try {
+                    bundle.putString("body", data.getString("alert"));
+                } catch (JSONException ignored) {}
+                try {
+                    bundle.putString("badge", data.getString("badge"));
+                } catch (JSONException ignored) {}
+                try {
+                    bundle.putString("group", data.getString("group"));
+                } catch (JSONException ignored) {}
+                try {
+                    bundle.putString("sound", data.getString("sound"));
+                } catch (JSONException ignored) {}
+            } catch (JSONException ignored) {
+                Log.d(LOGTAG, "Failed to parse raw data");
+            }
+        }
 
         try {
             final IPushNotification notification = PushNotification.get(getApplicationContext(), bundle);
